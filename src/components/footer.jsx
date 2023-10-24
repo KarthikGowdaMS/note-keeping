e36a03aae110ec82f5ae23fa6fc27406336e68c5
karthikgowdams03 <mskarthikgowda03@gmail.com>
2023-10-24 21:06:01 +0530

    initial commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    const year = new Date().getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright {year}</p>
+        </footer>
+    )
+}
---
+++ src/components/note.jsx
@@ -0,0 +1,8 @@
+export default function Note(){
+    return (
+        <div className="note">
+            <h1>Title</h1>
+            <p>content</p>
+        </div>
+    )
+}
