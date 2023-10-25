f3a2a74726e484d8aab30b898efa0c0550538629
karthikgowdams03 <mskarthikgowda03@gmail.com>
2023-10-25 16:12:44 +0530

    add notes and map method

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
-import Header from './components/header';
-import Footer from './components/footer';
-import Note from './components/note';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Note from './components/Note';
+import notes from './notes';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      {notes.map((note) => {
+        return <Note key={note.key} title={note.title} content={note.content} />
+      })
+      }
       <Footer />
     </div>
   );
