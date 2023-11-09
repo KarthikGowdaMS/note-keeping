bfe432f7bb830b675c26b982d6390a06e0721e45
karthikgowdams03 <karthikgowdams27@gmail.com>
2023-11-09 22:07:31 +0530

    Remove unused useEffect hook from App component.

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
