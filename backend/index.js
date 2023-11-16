c52e6bf7a28f0c9bb50dae9a57c2533e0e4f55e2
karthikgowdams03 <karthikgowdams27@gmail.com>
2023-11-16 15:59:13 +0530

    Remove origin URL from CORS configuration.

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,6 @@ mongoose.connect(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://witty-field-0607dc10f.4.azurestaticapps.net/',
     credentials: true,
   })
 );
