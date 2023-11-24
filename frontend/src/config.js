f76b2631878e1dc6effcd71a6cdcf8dee4a1c693
karthikgowdams03 <karthikgowdams27@gmail.com>
2023-11-24 23:14:37 +0530

    Update CORS configuration

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-// const BASE_URL='https://note-keeping-backend.karthikgowdams.com'
-const BASE_URL='http://localhost:5000'
+const BASE_URL='https://note-keeping-backend.karthikgowdams.com'
+// const BASE_URL='http://localhost:5000'
 
 export default BASE_URL;
