55be631d288e97b6767336c211902b96ffb2e0b7
Karthik Gowda M S <karthikgowdams27@gmail.com>
2024-10-26 22:57:42 +0530

    Update config.js
    Signed-off-by: Karthik Gowda M S <karthikgowdams27@gmail.com>

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-const BASE_URL='https://note-keeping-backend.karthikgowdams.com'
+const BASE_URL='https://note-keeping-backend.karthikgowdams.co'
 // const BASE_URL='http://localhost:5000'
 // const BASE_URL = 'http://192.168.0.103:5000';
 
